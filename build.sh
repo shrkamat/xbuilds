a39218ea98e7de23aa91996d636c8c6bb630c9b7
Shrinivas Kamath <skamath@synamedia.com>
2021-09-24 12:54:30 +0530

    adhear to CMAKE_INSTALL_PREFIX

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p build
-cmake -DCMAKE_TOOLCHAIN_FILE=$PWD/targets/aarch64.cmake -S lttng -B build
+cmake -DCMAKE_INSTALL_PREFIX=/NDS -DCMAKE_TOOLCHAIN_FILE=$PWD/targets/aarch64.cmake -S lttng -B build
