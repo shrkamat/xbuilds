649b93c8e04ed8606eb6a83d48ae16a39e26cf62
Shrinivas Kamath <skamath@synamedia.com>
2021-10-06 21:54:55 +0530

    heaptrack cross compilation

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+
 mkdir -p build
-cmake -DCMAKE_INSTALL_PREFIX=/NDS -DCMAKE_TOOLCHAIN_FILE=$PWD/targets/aarch64.cmake -S lttng -B build
+cmake -DCMAKE_INSTALL_PREFIX=/tools -DCMAKE_TOOLCHAIN_FILE=$PWD/targets/aarch64.cmake -S heaptrack -B build
